ccd9d19d49098e192954ecce9bbbfa90d6273d02
fbarrier <felix.barriere@gmail.com>
2022-11-14 14:43:01 -0500

    test docker-compose build

--- srcs/requirements/wordpress/conf/auto_config.sh
+++ srcs/requirements/wordpress/conf/auto_config.sh
@@ -4,7 +4,7 @@ RETURN=1
 while [$RETURN != 0]; do
 	echo "---------- wordpress is waiting for MariaDB -----------"
 	sleep 5
-	mysqladmin -u ${USERNAME} -p${PASSWORD}	-h mariadb ping > /dev/null 2>&1
+	mysqladmin -u ${SQL_USER} -p${SQL_PASSWORD} -h mariadb ping > /dev/null 2>&1
 	RETURN=$?
 done
 
