934ae0fcc2207db70998b26571650abcddccf84b
fbarrier <felix.barriere@gmail.com>
2022-11-16 06:12:22 -0500

    look for error2

--- srcs/requirements/wordpress/conf/wp_config.sh
+++ srcs/requirements/wordpress/conf/wp_config.sh
@@ -11,21 +11,13 @@ done
 
 echo "---------- Wordpress configurating -----------"
 
-if [! -f /var/www/wordpress/wp-config-sample.php && -f auto_config.sh];
-then
-   wp config create	--allow-root \
-   	&& --dbname=$SQL_DATABASE \
-   	&& --dbuser=$SQL_USER \
-   	&& --dbpass=$SQL_PASSWORD \
-   	&& --dbhost=mariadb:3306 --path='/var/www/wordpress'
-fi
-
-wp core install
-wp user create fbarrier --role='default'
+wp config create	--allow-root --dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$SQL_PASSWORD --dbhost=mariadb:3306 --path='/var/www/wordpress'
+wp core install 	--allow-root --user=$SQL_DATABASE --path='/var/www/wordpress'
+wp user create fbarrier --allow-root --role='default' --user_pass=$WP_PASSWORD --display_name=$WP_USERNAME 
 
 if [ -d /run/php ]
 then
-    mkdir -p [/run/php]
+    mkdir -p /run/php/php7.3
 fi
 
 exec /usr/sbin/php-fpm7.3 -F
